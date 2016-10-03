3b3e452a08d512e9cf872c1083e8b04fc18d52d6
nickto <nickto0x2c@gmail.com>
2016-10-03 20:52:20 +0200

    Added tests

---
+++ tests/testthat/test_knapsack_dynamic.R
@@ -0,0 +1,35 @@
+context("Test knapsack_dynamic")
+
+# test_that("Inputs", {
+# 
+# })
+
+
+# Test on small data
+knapsack_objects <- data.frame(w = c(1, 3, 4, 5), v = c(1, 4, 5, 7))
+test_that("Outputs", {
+  expect_equals(knapsack_dynamic(x = knapsack_objects, W = 7)$value, 9)
+  expect_equals(knapsack_dynamic(x = knapsack_objects, W = 7)$elements, c(2, 3))
+})
+
+# Test on large data
+knapsack_objects <- genKnapsack(2000)
+test_that("Outputs", {
+  expect_equals(knapsack_dynamic(x = knapsack_objects[1:8,], W = 3500)$value, 
+                16670)
+  expect_equals(knapsack_dynamic(x = knapsack_objects[1:8,], W = 3500)$elements,
+                c(5, 8))
+  expect_equals(knapsack_dynamic(x = knapsack_objects[1:12,], W = 3500)$value, 
+                16670)
+  expect_equals(knapsack_dynamic(x = knapsack_objects[1:12,], W = 3500)$elements,
+                c(5, 8))
+  expect_equals(knapsack_dynamic(x = knapsack_objects[1:8,], W = 2000)$value, 
+                15428)
+  expect_equals(knapsack_dynamic(x = knapsack_objects[1:8,], W = 2000)$elements,
+                c(3, 8))
+  expect_equals(knapsack_dynamic(x = knapsack_objects[1:12,], W = 2000)$value, 
+                15428)
+  expect_equals(knapsack_dynamic(x = knapsack_objects[1:12,], W = 2000)$elements,
+                c(3, 8))  
+})
+
