936d848fbaca8343e0abfdf1fd9d532acc1bb63b
nickto <nickto0x2c@gmail.com>
2016-10-03 19:50:58 +0200

    Corrected dynamic

--- R/knapsack_dynamic.R
+++ R/knapsack_dynamic.R
@@ -1,27 +1,33 @@
 knapsack_dynamic <- function(x, W) {
   
-  
-  # Following the pseudocode on wiki.
-  # Nothe that there they use an array which has a 0-th element. We don't, 
-  # thus we start i from 2, conrinue it to n+1 and when we access elements of x
-  # we substract 1 from i.
   n <- nrow(x)
   
-  m <- matrix(NA, nrow = n + 1, ncol = W)
-  m[1, ] <- 0
+  # Pre-allocate matrix
+  m <- matrix(NA, nrow = n, ncol = W + 1)
+  m[, 1] <- 0
   
-  for (i in 1:n+1) {
-    for (j in 1:W) {
-      if(x$w[i - 1] > j) {
-        m[i, j] <- m[i - 1, j]
+  # Identify max values
+  for (e in 1:n) { # up to this item is allowed to use
+    for (w in 2:(W + 1)) { # max allowed weight in these iteration
+      if (x$w[e] > w - 1) {
+        m[e, w] <- m[e - 1, w]
       } else {
-        m[i, j] <- max(m[i - 1, j], m[i - 1, j - x$w[i - 1]] + x$v[i - 1])
+        # max(m[e - 1, w - x$w[e]], 0) is needed, because if we are in the 
+        # first row, then e - 1 returns numeric(0), but numeric(0) plus any
+        # number is numeric(0). Thus, m[e, w - 1] will always be larger. Which 
+        # is not what we want.
+        m[e, w] <- max(m[e - 1, w], 
+                       max(m[e - 1, w - x$w[e]], 0) + x$v[e])
       }
     }
   }
   
-  res <- list()
-  res$value <- m[i, j]
-  res$elemnts <- "NOT IMPLEMENTED YET"
-  return(res)
+  # Indetify elements
+  
+  
+  
+  solution <- list()
+  solution$value <- m[e, w]
+  solution$elemnts <- "NOT IMPLEMENTED"
+  return(solution)
 }
